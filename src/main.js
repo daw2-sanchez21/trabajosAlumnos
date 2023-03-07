13ca1cebaccbd7fbc6f79e9cfcf89572744c677e
Carlos Sanchez <sanchezhernandezcarlos@fpllefia.com>
2023-03-07 11:12:09 +0100

    inicial

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import {home}  from  "./vistas/home";
+import {header}  from  "./componentes/header";
+import {footer}  from  "./componentes/footer";
+
+document.querySelector('main').innerHTML = home.template;
+document.querySelector('header').innerHTML = header.template;
+document.querySelector('footer').innerHTML = footer.template;
