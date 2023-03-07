5c9271598585a503b1fec9c5908baa03d907f235
Carlos Sanchez <sanchezhernandezcarlos@fpllefia.com>
2023-03-07 11:59:31 +0100

    Rama para probar supabase

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from 'path'
 
 export default {
     root: path.resolve(__dirname, 'src'),
+    base: './',
     resolve: {
         alias: {
           '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
